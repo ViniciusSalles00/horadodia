3c4d50625a4325c09c041712ce92154e6e48c0ef
Vinícius Salles <viniciusallesm@gmail.com>
2023-02-11 10:54:38 -0300

    mudançadiretorio

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-function carregar () {
+function carregar() {
         const msg = window.document.getElementById ('msg')
         let img = window.document.getElementById ('imagem')
         const data = new Date()
@@ -9,13 +9,13 @@ function carregar () {
         
 
         if (hora >= 0 && hora < 12) {
-                img.src = '/img/manha.png'
+                img.src = 'img/manha.png'
                 document.body.style.background = '#e2cd9f'
         } else if (hora >= 12 && hora <= 18) {
-                img.src = '/img/tarde.png'
+                img.src = 'img/tarde.png'
                 document.body.style.background = '#b9846f'
         } else {
-                img.src = '/img/noite.png'
+                img.src = 'img/noite.png'
                 document.body.style.background = '#515154'
         }
 }
